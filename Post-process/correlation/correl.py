fab7fc41139dce23ca8a31c81469d45a86f45b57
michgz <42281265+michgz@users.noreply.github.com>
2019-01-24 22:27:00 +0000

    Create a simple spreadsheet

--- Post-process/correlation/correl.py
+++ Post-process/correlation/correl.py
@@ -1,14 +1,20 @@
+# Run with Python 2.7
+##
+
 
 import datetime
 import os
+import zipfile
+import shutil
 
 
 
+runningPath = os.path.dirname(os.path.abspath(__file__))
 
 
 
 adxl = []
-with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),'ADXL.csv'),'r') as f1:
+with open(os.path.join(runningPath,'ADXL.csv'),'r') as f1:
  for s in f1:
   t = s.strip().split('\t')
   a1 = datetime.datetime.strptime(t[0], '%d/%m/%Y %H:%M:%S')
@@ -17,7 +23,7 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),'ADXL.csv'),'r
   adxl.append([a1,a2,a3])
 
 lsm6 = []
-with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),'LSM6.csv'),'r') as f1:
+with open(os.path.join(runningPath,'LSM6.csv'),'r') as f1:
  for s in f1:
   t = s.strip().split('\t')
   a1 = datetime.datetime.strptime(t[0], '%d/%m/%Y %H:%M:%S')
@@ -75,3 +81,42 @@ for a1 in adxl:
 
 print adxl_idx
 
+
+
+theName = os.path.join(runningPath, '1.ods')
+
+
+if True:
+
+ # Now output to a file
+
+
+ ######################################################
+ ###  Write Header copperplate to the contents file  ##
+
+ with open(    runningPath +  "/content_local.xml", "w") as dest:
+  with open(    runningPath +  "/../template_content/1", "r") as source:
+   while True:
+    copy_buff = source.read(4096)
+    if not copy_buff:
+     break
+    dest.write(copy_buff)
+
+
+
+  dest.write(       "<office:body><office:spreadsheet><table:calculation-settings table:automatic-find-labels=\"false\"/>"     )
+  dest.write(       "<table:table table:name=\"Data\" table:style-name=\"ta\">"      )
+  dest.write(       "<table:table-row table:style-name=\"ro1\">"    )
+  dest.write(       "<table:table-cell office:value-type=\"string\" calcext:value-type=\"string\"><text:p>%s</text:p></table:table-cell>" % ("Hello!")   )
+  dest.write(       "</table:table-row>"     )
+  dest.write(       "</table:table>"     )
+  dest.write(       "</office:spreadsheet></office:body></office:document-content>" )
+
+
+ ##### Now write to the ZIP archive  ####
+ shutil.copy2(  runningPath + "/../template_content/3_1.ods", theName)
+ with zipfile.ZipFile(theName, "a") as z:
+  z.write(  runningPath +  "/content_local.xml", "content.xml", zipfile.ZIP_DEFLATED )
+  z.close()
+
+ os.remove(  runningPath +  "/content_local.xml")
